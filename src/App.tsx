eaae3b89e6531509bd3c15dd2745b232f7bca12e
Andrey Polulyakh <sQrun1217@mail.ru>
2026-05-16 23:09:12 +1000

    create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { Header } from "./components/Header";
+
 export const App = () => {
   return (
-    <>Landing</>
+    <Header />
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+export const Header = () => {
+  return (
+    <header className="px-8 py-4 flex justify-between items-center border-b-2">
+      <div className="flex items-center gap-3">
+        <div className="w-9 h-9 border-2 rounded-lg overflow-hidden">
+          <span className="block w-full h-1/3 bg-background"></span>
+          <span className="block w-full h-1/3 bg-primary"></span>
+          <span className="block w-full h-1/3 bg-secondary"></span>
+        </div>
+        <span className="uppercase text-2xl font-black">Свайп·курсант</span>
+      </div>
+      <nav>
+        <ul className="flex gap-10 ">
+          <li>Отбор</li>
+          <li>Поступающим</li>
+          <li>Тест на роль</li>
+          <li>Вопросы</li>
+        </ul>
+      </nav>
+      <button className="bg-primary px-5 py-3 text-white text-sm rounded-full border-2 border-black 
+      shadow-[3px_3px_0] shadow-black font-bold">Подать заявку</button>
+    </header>
+  );
+}
